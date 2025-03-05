a9db0a95fc12cafbd35ac880393e84e80ebb90af
Varun Repo <varunbali345@gmail.com>
2025-03-05 19:52:14 +0530

    build the basic layout of the app just printed hello world

--- app/src/main/java/com/example/firstapplcation/MainActivity.kt
+++ app/src/main/java/com/example/firstapplcation/MainActivity.kt
@@ -1,49 +1,11 @@
 package com.example.firstapplcation
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.firstapplcation.ui.theme.FirstApplcationTheme
-import androidx.compose.ui.unit.dp
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            FirstApplcationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 }
-
-@Composable // This Composable function takes some input and generates what's shown on the screen.
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Red) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier.padding(24.dp)
-        )
-    }
-}
-
-@Preview(showBackground = true) //The @Preview annotation tells Android Studio that this composable should be shown in the design view of this file.
-@Composable
-fun GreetingPreview() {
-    FirstApplcationTheme {
-        Greeting("varun")
-    }
-}
-
